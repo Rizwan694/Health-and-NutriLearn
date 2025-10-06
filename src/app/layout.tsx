6912aac7a277db7480aadb353aecb3a81d89a0ef
Rizwan694 <faizafatima0609@gmail.com>
2025-10-06 09:51:42 +0000

    savefile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'Health and NutriLearn',
   description: 'Your guide to robust health and nutrition.',
+other: {
+  'google-adsense-account': 'content="ca-pub-5240590917046948',
+},
 };
 
 export default function RootLayout({
